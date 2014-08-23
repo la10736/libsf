46307a29125a8c79e5d8483bd6185a2b9ac81d15
Michele d'Amico <michele.damico@gmail.com>
2014-08-23 22:08:35 +0200

    All tests

---
+++ libsf/python/test/test_ALL.py
@@ -0,0 +1,15 @@
+'''
+Created on 23/ago/2014
+
+@author: michele
+'''
+
+from test_SizeGraph import *
+from test_UnionFind import *
+from test_H0Tree import *
+from test_SizeFunction import *
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
