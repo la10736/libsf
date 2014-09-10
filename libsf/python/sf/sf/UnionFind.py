15019a8e244e1cc6824c1b9d7f64e43ca0a22e64
la10736 <michele.damico@gmail.com>
2014-09-10 16:34:47 +0200

    Halving implementation of UnionFind and root should be the node with None parent

--- libsf/python/sf/sf/UnionFind.py
+++ libsf/python/sf/sf/UnionFind.py
@@ -54,54 +54,29 @@ class Set(object):
 
 
 '''
-unionfind.py
-
-A class that implements the Union Find data structure and algorithm.  This
-data structure allows one to find out which set an object belongs to, as well
-as join two sets.
-
 The algorithm's performance, given m union/find operations of any ordering, on
 n elements has been shown to take log* time per operation, where log* is
 pronounced log-star, and is the INVERSE of what is known as the Ackerman
 function, which is given below:
 A(0) = 1
 A(n) = 2**A(n-1)
-
-I include the functions to be complete.  Note that we can be 'inefficient'
-when performing the inverse ackerman function, as it will only take a maximum
-of 6 iterations to perform; A(5) is 65536 binary digits long (a 1 with 65535
-zeroes following).  A(6) is 2**65536 binary digits long, and cannot be
-represented by the memory of the entire universe.
-
-
-The Union Find data structure is not a universal set implementation, but can
-tell you if two objects are in the same set, in different sets, or you can
-combine two sets.
-ufset.find(obja) == ufset.find(objb)
-ufset.find(obja) != ufset.find(objb)
-ufset.union(obja, objb)
-
-
-This algorithm and data structure are primarily used for Kruskal's Minimum
-Spanning Tree algorithm for graphs, but other uses have been found.
-
-August 12, 2003 Josiah Carlson
 '''
 
 class UnionFind_by_rank(Set):
+    """The union by rank and halving implementation that collapse to the root one over two node in
+    the path to the root"""
     def __init__(self, context=None):
         '''Create an empty union find data structure.'''
         super(UnionFind_by_rank,self).__init__(context)
         self._rank = 1
-        '''Trick good find implementation'''
-        self._parent = self 
 
     def _find_impl(self):
-        stk = [self]
-        root = self._parent
-        '''root is the last element in the stack => Cycle found => the root'''
-        while root is not stk[-1]:
+        stk = []
+        root = self
+        while root._parent is not None and root._parent._parent is not None :
             stk.append(root)
+            root = root._parent._parent
+        if root._parent is not None:
             root = root._parent
         for p in stk:
             p._parent = root
