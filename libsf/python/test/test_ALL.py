16f892bb7292656b71e773ae5579ed9b539e4b56
Michele d'Amico <michele.damico@gmail.com>
2014-10-12 21:32:50 +0200

    Trick for recgnize test_ALL as unit test file

--- libsf/python/test/test_ALL.py
+++ libsf/python/test/test_ALL.py
@@ -4,10 +4,11 @@ Created on 23/ago/2014
 @author: michele
 '''
 
-from test_SizeGraph import *
-from test_UnionFind import *
-from test_H0Tree import *
 from test_SizeFunction import *
+
+
+class Test_Dummy(unittest.TestCase):
+    pass
 
 
 if __name__ == "__main__":
